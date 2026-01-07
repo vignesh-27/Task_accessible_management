47a0073c8415f9872ae43ceca6b78869be072176
Vignesh S <vigneshrevathy27@gmail.com>
2026-01-07 11:42:23 +0530

    Initial commit for task accessible management

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TaskboardComponent } from './pages/taskboard/taskboard.component';
+
+export const routes: Routes = [
+    {path: '', component: TaskboardComponent}
+];
---
+++ src/app/pages/taskboard/taskboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+
+@Component({
+  selector: 'app-taskboard',
+  imports: [NavComponent],
+  templateUrl: './taskboard.component.html',
+  styleUrl: './taskboard.component.scss'
+})
+export class TaskboardComponent {
+
+}
